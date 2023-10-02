1b5bb41c7698d21cd0988cc9b17c6c9284087f47
Rishabh Gulati <gulati.r7121@gmail.com>
2023-10-02 11:01:12 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,38 @@
+# n = 100# = > we are assigning something
+# if n < 100: # now if I want to check wether n has 100 or not [== ]
+#   print("It is less than")
+# else:
+#   print("It's not ") # this condition will only work if above condition is not true
+
+
+# Q4. Write a program to check whether a person is eligible for voting or not. (accept age from user)
+# # Show Answer
+
+# n = int(input("Enter your number : "))
+# if n %2 ==0: # if I divide the number by 2 
+#    print("Number is even")
+# else:
+#   print("Number is Odd")
+
+# n = 20
+# j = 31
+# if n ==20 or j ==30: # and both conditions have to be true
+# # OR one of the condition have to be true
+#   print("Hello")
+# else:
+#   print("BAyyi")
+
+
+#Write a program to accept a number from 1 to 7 and display the name of the day like 1 for Sunday , 2 for Monday and so on.
+
+
+# this means I am storing 20 as data inside num1 
+# = > is used to store something
+
+# == > to check if num1 has 20 or not
+
+# Accept any city from the user and display monument of that city.
+#                   City                                 Monument
+#                   Delhi                               Red Fort
+#                   Agra                                Taj Mahal
+#                   Jaipur                              Jal Mahal
